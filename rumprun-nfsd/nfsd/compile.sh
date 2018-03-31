582d03cc463b5cafab2ffcf2189fdaa4c1935ba9
Ruslan Nikolaev <rnikola@vt.edu>
2018-03-30 22:15:47 -0400

    Up-to-date scripts & README

--- rumprun-nfsd/nfsd/compile.sh
+++ rumprun-nfsd/nfsd/compile.sh
@@ -4,6 +4,7 @@ export PATH="${PATH}:$(pwd)/../rumprun/bin"
 
 (cd src && x86_64-rumprun-netbsd-gcc -Wall -O2 -pthread -o ../nfsd rpcbind/rpcbind.c rpcbind/check_bound.c rpcbind/pmap_svc.c rpcbind/rpcb_stat.c rpcbind/rpcb_svc_4.c rpcbind/rpcb_svc.c rpcbind/rpcb_svc_com.c rpcbind/security.c rpcbind/util.c rpcbind/warmstart.c mountd/get_net.c mountd/mountd.c nfsd/nfsd.c -DPORTMAP -DRUMPRUN)
 
+mkdir -p rootfs/var/run
 x86_64-rumprun-netbsd-cookfs -s 1 rootfs.fs rootfs
 
 rumprun-bake -m "add rootfs.fs" xen_pv nfsd.bin nfsd
--- rumprun-nfsd/nfsd/start.sh
+++ rumprun-nfsd/nfsd/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export PATH="${PATH}:$(pwd)/../rumprun/bin"
+export PATH="${PATH}:/usr/local/bin:/usr/local/sbin:$(pwd)/../rumprun/bin"
 
 rumprun xen -i -b nfsd.img,/disk -I nfsd0,xenif,bridge=xenbr0,vifname=vif-nfs -W nfsd0,inet,static,192.168.0.2/24 nfsd.bin
